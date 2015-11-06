938426255666ab857f2787782a8109819bd8f087
Brandon Raffin <braffin@carroll.edu>
2015-11-06 15:03:04 -0700

    Added most of the Game in the Console. Enum for game status, having a hand greater than 21 ends the game and you lose. Suits are not up yet, but values are passed correctly.

---
+++ BigProject2/src/project2/CardCreator.java
@@ -0,0 +1,55 @@
+package project2;
+
+import java.security.SecureRandom;
+
+import javax.swing.JOptionPane;
+
+public class CardCreator {
+		
+
+		public int CardValue(int playerhandvalue) {
+		int cardvalue=0;
+		SecureRandom cardpicker = new SecureRandom();
+		
+		
+		String [] facevalue = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+		
+		
+		int drawface = cardpicker.nextInt(12);
+		
+		System.out.println("Player is dealed the " + facevalue[drawface]);
+		
+		switch (facevalue[drawface]){
+		case "Ace": {cardvalue=1; break;}//case end
+		case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "10":{cardvalue=drawface+1;break;}//end case
+		case "Jack": case "Queen": case "King": {cardvalue=10;}//end case
+		}//switch end
+		
+		
+		return(cardvalue);}//end CardValue
+
+		
+		
+		
+		
+		public String CardFace() {
+		String cardface;
+		SecureRandom cardpicker = new SecureRandom();
+		
+		
+		String [] suit = {"Hearts","Clubs","Spades","Diamonds"};
+		
+		int drawsuit = cardpicker.nextInt(3);
+		
+		
+		
+		
+		return (cardface);}//end CardFace
+		
+		//Hit or Stay Buttons JOPtionPanes
+		public int HitStay(){
+			String[] buttons = {"Hit", "Stay"};
+			int hitcommand = JOptionPane.showOptionDialog(null,"What do you want to do?","Title",
+					JOptionPane.PLAIN_MESSAGE, 3, null, buttons, buttons[0]);
+		return(hitcommand);}//end HitStay method
+}//end class CardCreator
--- BigProject2/src/project2/Driver.java
+++ BigProject2/src/project2/Driver.java
@@ -4,14 +4,51 @@ import java.util.Random;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class Driver {
+final class Driver {
 
+	private enum Status {CONTINUE, BUST, WON};
+	private enum PlayAgain {YES, NO};
 	
 	public static void main(String[] args){
-		DeckOfCards myDeck = new DeckOfCards();
-		myDeck.shuffle();
+		//variables block
+		int playerhandvalue=0; 	//total value of all cards in players hand
+		int dealerhandvalue=0;	//total value of all cards dealt to the dealer
+		int cardvalue;			//value of card dealt to user
+		int hitcommand;			//0 means hit, 1 means stay
+		int losecount=0;			//counts of games lost
+		int wincount=0;			//counts of games won
+		
+		Status gamestatus;		//gamestatus for player to continue game
+		PlayAgain againstatus;	//status for player to play again
+		CardCreator getcardface = new CardCreator(); //reference to get to the class CardCreator
+		
+
+		
 		PlayPrompt();
-		System.out.println("Player is Dealt: " + myDeck.dealCard() + "\n" + myDeck.dealCard());
+		do{
+		cardvalue = getcardface.CardValue(playerhandvalue);
+		playerhandvalue = playerhandvalue + cardvalue;
+		System.out.println("the variable cardvalue is " + cardvalue);
+		System.out.println("the variable playerhandvalue is " + playerhandvalue);
+		
+		if(playerhandvalue>21){gamestatus = Status.BUST;break;}//end if
+		
+		hitcommand = getcardface.HitStay();
+		System.out.println("hitcommand is " + hitcommand);
+		gamestatus = Status.CONTINUE;}//end do loop
+		while(hitcommand==0);
+		
+		System.out.println("Players Total Hand value is: " + playerhandvalue);
+		
+		while(gamestatus==Status.CONTINUE){System.out.println("The Game is Continuing");
+		gamestatus = Status.WON;break;}
+		
+		while(gamestatus==Status.WON){wincount++;System.out.println("Player Wins");break;}
+		
+		while(gamestatus==Status.BUST){losecount++;System.out.println("Player Loses");break;}
+		
+		System.out.println("Games Won: " + wincount + "\nGames Lost: " + losecount);
+		
 //		int playagain =PlayAgainPrompt();
 //		System.out.println(playagain);
 	}//end void main
@@ -22,8 +59,10 @@ public class Driver {
 		String[] buttons = {"Yes", "No"}; 
 		int play = JOptionPane.showOptionDialog(null, "Do you want to play Blackjack?","Play Prompt", 
 				JOptionPane.PLAIN_MESSAGE, 3, null, buttons, buttons[0]);
-		if(play==1){System.exit(0);}		
+		if(play==1){System.exit(0);}
+		//JOptionPane.; EXIT ON CLOSE
 	}//end PlayGame method
+	
 	
 	
 	
